cba3086749e1d1d3b661bbf0a297e93c3dfe3b6b
Indre <indre.liaukuviene@gmail.com>
2019-12-10 20:55:22 +0200

    hangman - ok

--- wordlistas.py
+++ wordlistas.py
@@ -1,14 +1,17 @@
 import random
-WORDLIST = 'wordlist.txt'
+
+WORDLIST = 'zodziukai.txt'
+
 
 def get_random_word(min_word_length):
     words = []
     with open(WORDLIST, 'r') as f:
         for word in f:
             if '(' or ')' in word:
-                continue  # Skip the word because it contains parentheses.
+                continue
             word = word.strip().lower()
             if len(word) < min_word_length:
-                continue  # Skip the word because it is too short.
+                continue
             words.append(word)
     return random.choice(words)
+
